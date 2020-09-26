568548cad9fe28ff638e0f1b3319d13d94f2e0c3
Jean Bispo <jean.bispo@sanar.com>
2020-09-26 01:57:21 -0300

    fix

--- index.rb
+++ index.rb
@@ -20,9 +20,7 @@ module ChatDemo
         redis_sub = Redis.new(host: uri.host, port: uri.port, password: uri.password)
         redis_sub.subscribe(CHANNEL) do |on|
           on.message do |channel, msg|
-            if msg.include? "response"
-              @clients.each {|ws| ws.send(msg) }
-            end
+            @clients.each {|ws| ws.send(msg) }
           end
         end
       end
