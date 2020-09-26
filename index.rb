2be140f9d7c201dd471fce610295f3b07923ed09
Jean Bispo <jean.bispo@sanar.com>
2020-09-25 23:46:48 -0300

    funcionando

--- index.rb
+++ index.rb
@@ -1,39 +1,41 @@
-require 'em-websocket'
+require 'faye/websocket'
 
-# if ARGV.size != 2
-#   $stderr.puts("Usage: ruby index.rb ACCEPTED_DOMAIN PORT")
-#   exit(1)
-# end
-
-# $stderr.puts("Connected to #{ARGV[0]}:#{ARGV[1]}")
 module ChatDemo
   class ChatBackend
+
+    KEEPALIVE_TIME = 15 # in seconds
+    CHANNEL        = "chat-demo"
+
     def initialize(app)
       @app = app
+      @clients = []
     end
 
     def call(env)
-      $stderr.puts(env['SERVER_NAME'], env['SERVER_PORT'])
-      EM.run {
-        EM::WebSocket.run(:host => env['SERVER_NAME'], :port => env['SERVER_PORT']) do |ws|
-          ws.onopen { |handshake|
-            puts "WebSocket connection open"
-
-            # Access properties on the EM::WebSocket::Handshake object, e.g.
-            # path, query_string, origin, headers
-
-            # Publish message to the client
-            ws.send "Hello Client, you connected to server"
-          }
-
-          ws.onclose { puts "Connection closed" }
-
-          ws.onmessage { |msg|
-            puts "Recieved message: #{msg}"
-            ws.send "Pong: #{msg}"
-          }
+      if Faye::WebSocket.websocket?(env)
+        ws = Faye::WebSocket.new(env, nil, {ping: KEEPALIVE_TIME })
+        ws.on :open do |event|
+          p [:open, ws.object_id]
+          @clients << ws
         end
-      }
+
+        ws.on :message do |event|
+          p [:message, event.data]
+          # @redis.publish(CHANNEL, sanitize(event.data))
+        end
+
+        ws.on :close do |event|
+          p [:close, ws.object_id, event.code, event.reason]
+          @clients.delete(ws)
+          ws = nil
+        end
+
+        # Return async Rack response
+        ws.rack_response
+
+      else
+        @app.call(env)
+      end
   end
 end
 end
